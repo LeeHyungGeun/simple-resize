6ab4f262370d43c1485730f98f607fb601e8c4b3
leehyunggeun <lhg4031@gmail.com>
2019-05-19 23:45:03 +0800

    change mainfile as index

--- configs/paths.js
+++ configs/paths.js
@@ -3,7 +3,7 @@ const absolutePath = _path => path.join(__dirname, '..', _path)
 
 module.exports = {
   context: absolutePath('src'),
-  app: absolutePath('src/app.js'),
+  app: absolutePath('src/index.js'),
   dist: absolutePath('dist'),
   public: absolutePath('public'),
   indexHtml: absolutePath('public/index.html'),
