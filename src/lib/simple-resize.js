06bed33af28ef387d3511df14194047dd053bccf
leehyunggeun <lhg4031@gmail.com>
2019-05-30 00:16:40 +0800

    change  as this

--- src/lib/simple-resize.js
+++ src/lib/simple-resize.js
@@ -1,32 +1,52 @@
 // import 'core-js/shim'
 // import 'regenerator-runtime/runtime'
 import watType from 'wat-type'
-
 import component from './component'
 
-const simpleResize = function simpleResize($elm) {
-  const $self = this
-  if (!watType.isHTMLElement($elm) && !watType.isString($elm)) {
-    return new Error('$elm should be string or HTMLElement.')
-  }
+// const simpleResize = function simpleResize($elm) {
+//   const $self = this
+//   if (!watType.isHTMLElement($elm) && !watType.isString($elm)) {
+//     return new Error('$elm should be string or HTMLElement.')
+//   }
 
-  if (watType.isHTMLElement($elm)) {
-    $self.$elm = $elm
-  } else {
-    $self.$elm = document.querySelector($elm)
-  }
+//   if (watType.isHTMLElement($elm)) {
+//     $self.$elm = $elm
+//   } else {
+//     $self.$elm = document.querySelector($elm)
+//   }
 
-  if (!$self.$elm) {
-    return
-  }
+//   if (!$self.$elm) {
+//     return
+//   }
 
-  component($self, $self.$elm)
+//   component($self, $self.$elm)
+// }
+
+class SimpleResize {
+  constructor($elm) {
+    this.$elm = $elm
+    if (!watType.isHTMLElement($elm) && !watType.isString($elm)) {
+      return new Error('$elm should be string or HTMLElement.')
+    }
+  
+    if (watType.isHTMLElement($elm)) {
+      this.$elm = $elm
+    } else {
+      this.$elm = document.querySelector($elm)
+    }
+  
+    if (!this.$elm) {
+      return
+    }
+  
+    component(this, this.$elm)
+  }
 }
 
 // if (typeof window === 'object') {
 //   window.simpleResize = simpleResize
 // }
 export {
-  simpleResize
+  SimpleResize
 }
-export default simpleResize
+export default SimpleResize
